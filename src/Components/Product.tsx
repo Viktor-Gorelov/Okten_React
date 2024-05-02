3ff8a3eed3f5a29c1ce33f74963638cc9b666c28
Viktor-Gorelov <viktor15gorelov@gmail.com>
2024-05-02 13:44:41 +0300

    Homework 1

---
+++ src/Components/Product.tsx
@@ -0,0 +1,36 @@
+import React, {FC} from 'react';
+
+export interface IProductProps{
+    id: number,
+    title: string,
+    description: string,
+    price: number,
+    discountPercentage: number,
+    rating: number,
+    stock: number,
+    brand: string,
+    category: string,
+    thumbnail: string,
+    images: string[]
+}
+export type IProductTypeProps = IProductProps &{children?: React.ReactNode};
+const Product: FC<IProductTypeProps> = ({id,title,description,price,
+                                     discountPercentage,rating, stock, brand,
+                                     category, thumbnail, images}) => {
+    return (
+        <div>
+            <h2> №{id}. Title: {title}. </h2>
+            <p>Description: {description}.</p>
+            <h2>Price: {price}. DiscountPercentage: {discountPercentage}. Rating: {rating}. </h2>
+            <h2>Stock: {stock}. Brand: {brand}. Category: {category}.</h2>
+            <img src={thumbnail} alt={title}></img>
+            <img src={images[0]} alt={title}></img>
+            <img src={images[1]} alt={title}></img>
+            <img src={images[2]} alt={title}></img>
+
+
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/Components/Products.tsx
@@ -0,0 +1,24 @@
+import React, {FC, useEffect, useState} from 'react';
+import product, {IProductProps, IProductTypeProps} from "./Product";
+import Product from "./Product";
+
+const Products: FC = () => {
+    const [products, setProducts] = useState<IProductProps[]>([])
+    useEffect(() => {
+        fetch('https://dummyjson.com/products')
+            .then(value => value.json())
+            .then(({products}) => {
+                setProducts(products);
+            });
+    },[])
+    return (
+        <div>
+            {
+                products
+                    .map(({id,title, description, price, discountPercentage, rating, stock, brand, category, thumbnail, images}: IProductProps) => (<Product key={id} id={id} title={title} description={description} price={price} discountPercentage={discountPercentage} rating={rating} stock={stock} brand={brand} category={category} thumbnail={thumbnail} images={images}/>))
+            }
+        </div>
+    );
+};
+
+export default Products;
